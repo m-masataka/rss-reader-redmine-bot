15af926d04a25b84413b1d925f4817f9d13adfe4
m-masataka <m.mizukoshi.wakuwaku@gmail.com>
2020-03-26 13:06:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# RSS Reader Redmine Bot
+This is a simple implementation of a RSS reader bot for Redmine written in golang.
+( for redmine in private network)
+
+## How To Config
+First, clone this project then build code.
+```
+# clone ....
+# cd go-rss-reader-redmine-bot
+# go get -d
+# go build rss_reader_redmine_bot.go
+```
+
+Next edit redmine_rss.conf.
+
+Example
+```
+# This tool logging the entry id that already sended.
+# comp_file_path define the id logging file path.
+comp_file_path = "./fn/"
+
+
+# Setting projects that you want to poll changes and notify.
+[[projects]]
+# Setting the project id. No duplication.
+id = "sample-project01_activity"
+# rss(atom) url
+url = "https://redmine.org/projects/sample-project-01/activity.atom?key=xxxxxx"
+  [projects.slack]
+  # bot_name
+  bot_name = "bot_name"
+  # webhook_url
+  webhook_url = "https://hooks.slack.com/services/xxxxxxxx"
+  # channel(post to) and bot's icon
+  channel = "general"
+  icon = "pig"
+
+[[projects]]
+.....
+```
+
+Dryrun
+At the first time, this script poll and send the all available entries.
+So please try dryrun to loggin current entry ( exec with --dryrun=true flag).
+```
+# ./rss_reader_redmine_bot --dryrun --config.file=./redmine_rss.conf
+```
+
+Setting cron to exec this script (without --dryrun flag)
+
+# Contributing
+Please submit PRs.
---
+++ rss_reader_redmine_bot.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io"
+	"strings"
+	"net/http"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/BurntSushi/toml"
+	"github.com/mmcdole/gofeed/atom"
+	"github.com/ashwanthkumar/slack-go-webhook"
+	"github.com/lunny/html2md"
+)
+
+type Config struct {
+	CompFilePath  string `toml:"comp_file_path"`
+	Projects []Project
+}
+
+type Project struct {
+	Url   string `toml:"url"`
+	Id    string `toml:"id"`
+	Slack Slack  `toml:"slack"`
+}
+
+type Slack struct {
+	WebhookUrl string `toml:"webhook_url"`
+	Channel    string `toml:"channel"`
+	BotName    string `toml:"bot_name"`
+	Icon       string `toml:"icon"`
+}
+
+var (
+	logger log.Logger
+	configFile = kingpin.Flag("config.file", "Config file path").Required().String()
+	redmineProxy = kingpin.Flag("redmine.proxy", "Set Proxy to redmine server").Default("").String()
+	slackProxy = kingpin.Flag("slack.proxy", "Set Proxy to slack api").Default("").String()
+	dryrun = kingpin.Flag("dryrun", "Set Proxy to slack api").Bool()
+)
+
+func main(){
+	// Setting logger
+	w := log.NewSyncWriter(os.Stdout)
+	logger = log.NewLogfmtLogger(w)
+	format := log.TimestampFormat(
+		func() time.Time { return time.Now().UTC() },
+		time.RFC3339Nano,
+        )
+	logger = log.With(logger, "timestamp", format, "caller", log.DefaultCaller)
+	logger = level.NewFilter(logger, level.AllowInfo())
+	level.Info(logger).Log("msg", "Start rss reader for redmine")
+
+	// parse args
+	kingpin.Parse()
+	// parse config
+	var conf Config
+	_, err := toml.DecodeFile(*configFile, &conf)
+	if err != nil {
+		level.Error(logger).Log("msg", "Config parse error", "error", err)
+		os.Exit(1)
+	}
+
+	// Start polling rss
+	for _, project := range conf.Projects {
+		err = pollProject(project, conf.CompFilePath)
+		if err != nil {
+			level.Error(logger).Log("msg", "Polling Aborted", "project_id", project.Id, "error", err)
+		}
+	}
+	// end
+	level.Info(logger).Log("msg", "Ended")
+}
+
+func pollProject (p Project, path string) (error) {
+	url := p.Url
+	bkFile := path + p.Id + ".log"
+
+	currentId, err := loadCurrentId(bkFile)
+	if err != nil {
+		level.Error(logger).Log("error", err)
+		return err
+	}
+
+	feed, err := parseBodyByUrl(url)
+	if err != nil {
+		level.Error(logger).Log("error", err)
+		return err
+	}
+	var entryList []atom.Entry
+	for _, entry := range feed.Entries {
+		if currentId == entry.ID {
+			break
+		}
+		entryList = append(entryList, *entry)
+	}
+	for i := len(entryList) -1; i >= 0 ; i-- {
+		if !(*dryrun) {
+			err = sendSlack(p.Slack, entryList[i])
+			if err != nil {
+				level.Error(logger).Log("error", err)
+				break
+			}
+		}
+			
+		err = logEntryId(bkFile, entryList[i].ID)
+		if err != nil {
+			level.Error(logger).Log("error", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func sendSlack(s Slack, e atom.Entry) (error) {
+	attachment := slack.Attachment {}
+	authors := ""
+	for _, person := range e.Authors {
+		authors = authors + person.Name
+	}
+	attachment.AddField(slack.Field { Title: "Authors", Value: authors, Short: true })
+	attachment.AddField(slack.Field { Title: "Content", Value: html2md.Convert(e.Content.Value)})
+	payload := slack.Payload {
+		Text: "<" + e.Links[0].Href + "|" + e.Title + ">",
+		Username: s.BotName,
+		Channel: "#" + s.Channel,
+		IconEmoji: ":" + s.Icon + ":",
+		Attachments: []slack.Attachment{attachment},
+		Markdown: true,
+	}
+	err := slack.Send(s.WebhookUrl, *slackProxy, payload)
+	if len(err) > 0 {
+		return fmt.Errorf("error: %s\n", err)
+	}
+	return nil
+}
+
+func loadCurrentId(path string) (string, error) {
+	line := ""
+	f, err := os.OpenFile(path, os.O_CREATE, 0666)
+	if err != nil {
+	    return line, err
+	}
+	defer f.Close()
+	byteSize := int64(500)
+	f.Seek(-1*(byteSize+1), 2)
+	b := make([]byte, (byteSize + 1))
+	for {
+		_, err := f.Read(b)
+		if err != nil {
+			if err != io.EOF {
+				return line, nil
+			}
+			break
+		}
+	}
+	line = string(b)
+	id := strings.Split(strings.Replace(line, "\r\n", "\n", -1), "\n")
+	if len(id) < 2 {
+		return line, nil
+	}
+	return id[len(id) - 2], nil
+}
+
+func logEntryId(path string, id string) (error) {
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+	    return err
+	}
+	defer f.Close()
+	fmt.Fprintln(f, id) 
+	return nil
+}
+
+func parseBodyByUrl(url string) (*atom.Feed, error) {
+	ap := atom.Parser{}
+	client := new(http.Client)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp != nil {
+		defer func() {
+			ce := resp.Body.Close()
+			if ce != nil {
+				err = ce
+			}
+		}()
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, fmt.Errorf("ERROR: HTTP Status Code %d", resp.StatusCode)
+	}	
+		
+	return ap.Parse(resp.Body)
+}
